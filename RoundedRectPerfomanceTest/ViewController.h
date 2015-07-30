df013687708a5c94327ce56fae747cb64fc15318
xuzhaocheng <zhaochengxu1991@gmail.com>
2015-07-30 11:06:57 +0800

    Initial Commit

---
+++ RoundedRectPerfomanceTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  RoundedRectPerfomanceTest
+//
+//  Created by xuzhaocheng on 15/7/30.
+//  Copyright (c) 2015年 nmlab. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ RoundedRectPerfomanceTest/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  RoundedRectPerfomanceTest
+//
+//  Created by xuzhaocheng on 15/7/30.
+//  Copyright (c) 2015年 nmlab. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
